cb367397b9c4506a6b396fcd867ba21d02a7b0de
happybae <qo98333@naver.com>
2022-08-13 18:09:44 +0900

    media test

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,32 @@
 import type { NextPage } from 'next';
+import styled from '@emotion/styled';
+
+const Wrapper = styled.div`
+  @media (max-width: 767px) {
+    background-color: pink;
+    height: 100vh;
+  }
+`;
+
+const Tab = styled.nav`
+  ul {
+    display: flex;
+
+  }
+`;
 
 const Home: NextPage = () => {
   return (
-    <div>
-      
-    </div>
+    <Wrapper>
+      <Tab>
+        <ul>
+          <li>이벤트</li>
+          <li>사이드</li>
+          <li>음료</li>
+          <li>세트</li>
+        </ul>
+      </Tab>
+    </Wrapper>
   );
 }
 
