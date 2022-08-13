2e2ce4db82a2b6548ad2b3fe415e51d31ae41c20
happybae <qo98333@naver.com>
2022-08-14 02:13:57 +0900

    eslint & prettier setting

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,33 +1,46 @@
-import type { NextPage } from 'next';
-import styled from '@emotion/styled';
+import type { NextPage } from "next";
+import styled from "@emotion/styled";
 
 const Wrapper = styled.div`
-  @media (max-width: 767px) {
-    background-color: pink;
-    height: 100vh;
-  }
+	@media (max-width: 767px) {
+		background-color: pink;
+		height: 100vh;
+	}
 `;
 
 const Tab = styled.nav`
-  ul {
-    display: flex;
-
-  }
+	ul {
+		display: flex;
+	}
 `;
 
 const Home: NextPage = () => {
-  return (
-    <Wrapper>
-      <Tab>
-        <ul>
-          <li>이벤트</li>
-          <li>사이드</li>
-          <li>음료</li>
-          <li>세트</li>
-        </ul>
-      </Tab>
-    </Wrapper>
-  );
-}
+	return (
+		<Wrapper>
+			<Tab>
+				<ul>
+					<li>
+						<button>
+							<span>이벤트</span>
+						</button>
+					</li>
+					<li>
+						<button>
+							<span>사이드</span>
+						</button>
+					</li>
+					<li>
+						<button>
+							<span>음료</span>
+						</button>
+					</li>
+					<li>
+						<button>세트</button>
+					</li>
+				</ul>
+			</Tab>
+		</Wrapper>
+	);
+};
 
 export default Home;
