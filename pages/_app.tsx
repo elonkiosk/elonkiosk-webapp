6e25bf62dcfd5054b310d36f9b3f9e3c6d46995c
happybae <qo98333@naver.com>
2022-08-14 14:35:40 +0900

    fontawesome & home header(css yet)

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,17 @@
-import type { AppProps } from 'next/app';
+import type { AppProps } from "next/app";
 import { Global } from "@emotion/react";
-import { global } from '../styles/global';
+import { global } from "../styles/global";
+import { config } from "@fortawesome/fontawesome-svg-core";
+import "@fortawesome/fontawesome-svg-core/styles.css";
+config.autoAddCss = false;
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return ( 
-  <>
-    <Global styles={global}/>
-    <Component {...pageProps} />
-  </>
-  );
+	return (
+		<>
+			<Global styles={global} />
+			<Component {...pageProps} />
+		</>
+	);
 }
 
 export default MyApp;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,43 +1,94 @@
 import type { NextPage } from "next";
 import styled from "@emotion/styled";
+import React, { useEffect, useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faAngleRight, faAngleLeft } from "@fortawesome/free-solid-svg-icons";
 
 const Wrapper = styled.div`
 	@media (max-width: 767px) {
-		background-color: pink;
+		background-color: #f8f9fa;
 		height: 100vh;
 	}
 `;
 
 const Tab = styled.nav`
+	display: flex;
+	background-color: #fff;
 	ul {
 		display: flex;
 	}
 `;
 
 const Home: NextPage = () => {
+	const tempcategory = [
+		"이벤트",
+		"사이드",
+		"음료",
+		"세트",
+		"추가1",
+		"추가2",
+		"추가3",
+		"추가4",
+		"추가5",
+		"추가6",
+		"추가7",
+		"추가8",
+	];
+
+	const [category, setCategory] = useState<string[]>([]);
+	const [pivot, setPivot] = useState(0);
+	const [leftidx, setLeftIdx] = useState(0);
+	const [rightidx, setRightIdx] = useState(4);
+
+	// useEffect(() => {
+	// 	tempcategory.forEach(item => {
+	// 		if (category.length < 6) setCategory([...category, item]);
+	// 	});
+	// }, []);
+
+	const TabPlus = (event: React.MouseEvent<HTMLButtonElement>) => {
+		event.preventDefault();
+		//setPivot(prev => prev + 1);
+		if (rightidx < tempcategory.length - 1) {
+			setLeftIdx(prev => prev + 1);
+			setRightIdx(prev => prev + 1);
+		}
+	};
+
+	const TabMinus = (event: React.MouseEvent<HTMLButtonElement>) => {
+		event.preventDefault();
+		if (leftidx > 0) {
+			setLeftIdx(prev => prev - 1);
+			setRightIdx(prev => prev - 1);
+		}
+	};
+
+	const RenderTap = () => {
+		const result = [];
+		console.log(leftidx);
+		console.log(rightidx);
+		for (let i = leftidx; i <= rightidx; i++) {
+			result.push(
+				<li>
+					<button>
+						<span>{tempcategory[i]}</span>
+					</button>
+				</li>,
+			);
+		}
+		return result;
+	};
+
 	return (
 		<Wrapper>
 			<Tab>
-				<ul>
-					<li>
-						<button>
-							<span>이벤트</span>
-						</button>
-					</li>
-					<li>
-						<button>
-							<span>사이드</span>
-						</button>
-					</li>
-					<li>
-						<button>
-							<span>음료</span>
-						</button>
-					</li>
-					<li>
-						<button>세트</button>
-					</li>
-				</ul>
+				<button onClick={TabMinus}>
+					<FontAwesomeIcon icon={faAngleLeft} />
+				</button>
+				<ul>{RenderTap()}</ul>
+				<button onClick={TabPlus}>
+					<FontAwesomeIcon icon={faAngleRight} />
+				</button>
 			</Tab>
 		</Wrapper>
 	);
