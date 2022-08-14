31b283b02d574ab430a73929dc97ff8bd5c1708c
happybae <qo98333@naver.com>
2022-08-15 01:52:21 +0900

    Tab styling

--- pages/index.tsx
+++ pages/index.tsx
@@ -11,19 +11,61 @@ import {
 } from "@fortawesome/free-solid-svg-icons";
 import MenuItem from "../components/MenuItem";
 
+//━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+
 const Wrapper = styled.div`
 	@media (max-width: 767px) {
 		//background-color: #f8f9fa;
 		background-color: pink;
 		height: 100vh;
+		display: grid;
+		grid-template-rows: 1.5fr 11fr 1fr 2fr;
 	}
 `;
 
 const Tab = styled.nav`
-	display: flex;
+	display: grid;
+	grid-template-columns: 1fr 10fr 1fr;
 	background-color: #fff;
+	margin-top: 12px;
+
 	ul {
-		display: flex;
+		display: grid;
+		grid-template-columns: repeat(5, 1fr);
+	}
+
+	#Tab-arrow {
+		font-size: 20px;
+		background-color: transparent;
+		border: 0;
+		border-bottom: 1px solid black;
+		&:hover {
+			cursor: pointer;
+		}
+	}
+`;
+
+interface ITabItem {
+	index: number;
+	tabnum: number;
+}
+
+const TabItem = styled.button<ITabItem>`
+	width: 100%;
+	height: 100%;
+	border: 1px solid black;
+	border-bottom: ${props =>
+		props.index == props.tabnum ? "none" : "1px solid black"};
+	background-color: ${props =>
+		props.index == props.tabnum ? "#fff" : "#f39c12"};
+	&:hover {
+		cursor: pointer;
+	}
+
+	span {
+		color: ${props => (props.index == props.tabnum ? "#2f3640" : "#fff")};
+		font-size: 17px;
+		font-weight: bold;
 	}
 `;
 
@@ -32,6 +74,9 @@ const Menu = styled.main`
 	grid-template-columns: repeat(3, 1fr);
 	grid-template-rows: repeat(3, 1fr);
 	gap: 10px;
+	padding: 10px;
+	border: 0;
+	border-bottom: 1px solid black;
 `;
 
 const Slide = styled.div`
@@ -45,6 +90,24 @@ const Slide = styled.div`
 
 	#ispage {
 		color: red;
+	}
+`;
+
+const Bottom = styled.div`
+	width: 100%;
+	display: grid;
+	grid-template-columns: 2fr 1fr 1fr;
+
+	#total-result {
+		display: grid;
+		padding-right: 10px;
+		grid-template-rows: 1fr 1fr;
+		div {
+			display: flex;
+			justify-content: space-between;
+			text-align: center;
+			align-items: center;
+		}
 	}
 `;
 
@@ -96,6 +159,9 @@ const Home: NextPage = () => {
 	const [leftidx, setLeftIdx] = useState(0);
 	const [rightidx, setRightIdx] = useState(4);
 	const [pagenum, setPageNum] = useState(0);
+	const [totalprice, setTotalPrice] = useState(0);
+	const [totalnumber, setTotalNumber] = useState(0);
+	const [tabnum, setTabNum] = useState(0);
 
 	// useEffect(() => {
 	// 	tempcategory.forEach(item => {
@@ -142,9 +208,16 @@ const Home: NextPage = () => {
 		for (let i = leftidx; i <= rightidx; i++) {
 			result.push(
 				<li>
-					<button>
+					<TabItem
+						onClick={event => {
+							event.preventDefault();
+							setTabNum(i);
+						}}
+						index={i}
+						tabnum={tabnum}
+					>
 						<span>{tempcategory[i]}</span>
-					</button>
+					</TabItem>
 				</li>,
 			);
 		}
@@ -190,11 +263,11 @@ const Home: NextPage = () => {
 	return (
 		<Wrapper>
 			<Tab>
-				<button onClick={TabMinus}>
+				<button onClick={TabMinus} id="Tab-arrow">
 					<FontAwesomeIcon icon={faAngleLeft} />
 				</button>
 				<ul>{RenderTap()}</ul>
-				<button onClick={TabPlus}>
+				<button onClick={TabPlus} id="Tab-arrow">
 					<FontAwesomeIcon icon={faAngleRight} />
 				</button>
 			</Tab>
@@ -208,6 +281,24 @@ const Home: NextPage = () => {
 					<FontAwesomeIcon icon={faCircleArrowRight} />
 				</button>
 			</Slide>
+			<Bottom>
+				<div id="total-result">
+					<div>
+						<span>수량</span>
+						<span>{totalnumber}</span>
+					</div>
+					<div>
+						<span>금액</span>
+						<span>{totalprice}</span>
+					</div>
+				</div>
+				<button>
+					<span>장바구니</span>
+				</button>
+				<button>
+					<span>결제</span>
+				</button>
+			</Bottom>
 		</Wrapper>
 	);
 };
