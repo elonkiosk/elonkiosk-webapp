6a91d2566895f1bb3dfe5c8cb6df9660e4655455
chopinballadeno4 <qo98333@naver.com>
2022-10-11 12:19:13 +0900

    before using React !

--- pages/convenientmenu/[category].tsx
+++ pages/convenientmenu/[category].tsx
@@ -18,17 +18,6 @@
 // import ConvenientButton from "../components/ConvenientButton";
 // import { useRouter } from "next/router";
 
-// interface IGets {
-// 	//error: null;
-// 	food_number: number;
-// 	food_category: string;
-// 	store_id: number;
-// 	food_name: string;
-// 	price: number;
-// 	food_pic: string;
-// 	food_explanation: string;
-// }
-
 // const Wrapper = styled.div`
 // 	@media (max-width: 768px) {
 // 		background-color: var(--color-darkwhite);
@@ -168,24 +157,6 @@
 // 	font-weight: bold;
 // `;
 
-// const Footer = styled.div`
-// 	display: grid;
-// 	place-items: center;
-// 	background-color: aqua;
-// 	button {
-// 		box-sizing: border-box;
-// 		width: 80%;
-// 	}
-// `;
-
-// const Main = styled.div`
-// 	flex: 1;
-// 	display: grid;
-// 	grid-template-columns: repeat(2, 1fr);
-// 	grid-template-rows: repeat(5, 1fr);
-// 	background-color: antiquewhite;
-// `;
-
 // function menu() {
 // 	const [data, setData] = useState<IGets[]>([]);
 // 	const [loading, setLoading] = useState(true);
@@ -200,144 +171,6 @@
 // 	const [tabnum, setTabNum] = useState(0);
 // 	const [category, setCategory] = useState<Set<string>>(new Set());
 // 	const router = useRouter();
-
-// 	useEffect(() => {
-// 		RestGet();
-// 	}, []);
-
-// 	const RestGet = async () => {
-// 		try {
-// 			setError(null);
-// 			const response = await axios.get("/gets");
-// 			const categorySet = new Set<string>();
-// 			response.data.content?.map((item: IGets) => {
-// 				categorySet.add(item.food_category);
-// 			});
-// 			setRightIdx(categorySet.size);
-// 			setCategory(categorySet);
-// 			setData(response.data.content);
-// 			setLoading(false);
-// 		} catch (e) {
-// 			setError(e);
-// 		}
-// 	};
-
-// 	mock.onGet("/gets").reply(() => {
-// 		const gets = {
-// 			error: null,
-// 			content: [
-// 				{
-// 					food_number: 1,
-// 					food_category: "버거류",
-// 					store_id: 1004,
-// 					food_name: "치즈렐라와퍼",
-// 					price: 3500,
-// 					food_pic: "/static/buger1.png",
-// 					food_explanation: "시원한 아메리카노",
-// 				},
-// 				{
-// 					food_number: 2,
-// 					food_category: "버거류",
-// 					store_id: 1004,
-// 					food_name: "치즈렐라치킨버거",
-// 					price: 4000,
-// 					food_pic: "/static/buger2.png",
-// 					food_explanation: "따뜻한 카페라떼",
-// 				},
-// 				{
-// 					food_number: 3,
-// 					food_category: "버거류",
-// 					store_id: 1004,
-// 					food_name: "몬스터X",
-// 					price: 3000,
-// 					food_pic: "/static/buger3.png",
-// 					food_explanation: "따듯한 녹차",
-// 				},
-// 				{
-// 					food_number: 1,
-// 					food_category: "버거류",
-// 					store_id: 1004,
-// 					food_name: "통새우X",
-// 					price: 3500,
-// 					food_pic: "/static/buger4.png",
-// 					food_explanation: "시원한 아메리카노",
-// 				},
-// 				{
-// 					food_number: 2,
-// 					food_category: "버거류",
-// 					store_id: 1004,
-// 					food_name: "콰트로치즈X",
-// 					price: 4000,
-// 					food_pic: "/static/buger5.png",
-// 					food_explanation: "따뜻한 카페라떼",
-// 				},
-// 				{
-// 					food_number: 3,
-// 					food_category: "버거류",
-// 					store_id: 1004,
-// 					food_name: "기네스콰트로치즈와퍼",
-// 					price: 3000,
-// 					food_pic: "/static/buger6.png",
-// 					food_explanation: "따듯한 녹차",
-// 				},
-// 				{
-// 					food_number: 1,
-// 					food_category: "버거류",
-// 					store_id: 1004,
-// 					food_name: "기네스머쉬룸와퍼",
-// 					price: 3500,
-// 					food_pic: "/static/buger7.png",
-// 					food_explanation: "시원한 아메리카노",
-// 				},
-// 				{
-// 					food_number: 2,
-// 					food_category: "버거류",
-// 					store_id: 1004,
-// 					food_name: "기네스와퍼",
-// 					price: 4000,
-// 					food_pic: "/static/buger8.png",
-// 					food_explanation: "따뜻한 카페라떼",
-// 				},
-// 				{
-// 					food_number: 3,
-// 					food_category: "버거류",
-// 					store_id: 1004,
-// 					food_name: "몬스터와퍼",
-// 					price: 3000,
-// 					food_pic: "/static/buger9.png",
-// 					food_explanation: "따듯한 녹차",
-// 				},
-// 				{
-// 					food_number: 1,
-// 					food_category: "사이드",
-// 					store_id: 1004,
-// 					food_name: "아메리카노",
-// 					price: 3500,
-// 					food_pic: "이미지 주소",
-// 					food_explanation: "시원한 아메리카노",
-// 				},
-// 				{
-// 					food_number: 2,
-// 					food_category: "음료",
-// 					store_id: 1004,
-// 					food_name: "카페라떼",
-// 					price: 4000,
-// 					food_pic: "이미지 주소",
-// 					food_explanation: "따뜻한 카페라떼",
-// 				},
-// 				{
-// 					food_number: 3,
-// 					food_category: "세트",
-// 					store_id: 1004,
-// 					food_name: "녹차",
-// 					price: 3000,
-// 					food_pic: "이미지 주소",
-// 					food_explanation: "따듯한 녹차",
-// 				},
-// 			],
-// 		};
-// 		return [200, gets];
-// 	});
 
 // 	//━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━ isButtonClick
 // 	const TabPlus = (event: React.MouseEvent<HTMLButtonElement>) => {
@@ -474,11 +307,6 @@
 // 		event.preventDefault();
 // 	};
 
-// 	const goPrev = (event: React.MouseEvent<HTMLButtonElement>): void => {
-// 		router.back();
-// 		event.preventDefault();
-// 	};
-
 // 	const goMenu = (event: React.MouseEvent<HTMLButtonElement>): void => {
 // 		router.push("/convenientmenu");
 // 		event.preventDefault();
@@ -559,12 +387,267 @@
 // }
 
 // export default menu;
+
+import styled from "@emotion/styled";
+import axios from "axios";
 import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
+import ConvenientButton from "../../components/ConvenientButton";
+import ConvenientLayout from "../../components/ConvenientLayout";
+import ConvenientTitle from "../../components/ConvenientTitle";
+import Loading from "../../components/Loading";
+import AxiosMockAdapter from "axios-mock-adapter";
+
+const Main = styled.div`
+	flex: 1;
+	display: grid;
+	grid-template-columns: repeat(2, 1fr);
+	grid-template-rows: repeat(2, 1fr);
+	background-color: antiquewhite;
+`;
+
+const Footer = styled.div`
+	display: grid;
+	place-items: center;
+	background-color: aqua;
+	button {
+		box-sizing: border-box;
+		width: 80%;
+	}
+`;
+
+interface IGets {
+	//error: null;
+	food_number: number;
+	food_category: string;
+	store_id: number;
+	food_name: string;
+	price: number;
+	food_pic: string;
+	food_explanation: string;
+}
 
 function menu() {
+	const [loading, setLoading] = useState(false);
+
+	const [data, setData] = useState<IGets[]>([]);
+	const [error, setError] = useState<unknown>();
+	const mock = new AxiosMockAdapter(axios, { delayResponse: 500 });
+
+	const [leftidx, setLeftIdx] = useState(0);
+	const [rightidx, setRightIdx] = useState(4);
+	const [pagenum, setPageNum] = useState<number>(0);
+	const [totalprice, setTotalPrice] = useState(0);
+	const [totalnumber, setTotalNumber] = useState(0);
+	const [tabnum, setTabNum] = useState(0);
+	const [category, setCategory] = useState<Set<string>>(new Set());
 	const router = useRouter();
-	console.log(router.query);
-	return <div></div>;
+
+	const goPrev = (event: React.MouseEvent<HTMLButtonElement>): void => {
+		router.back();
+		event.preventDefault();
+	};
+
+	useEffect(() => {
+		RestGet();
+	}, []);
+
+	const RestGet = async () => {
+		try {
+			setError(null);
+			const response = await axios.get("/gets");
+			const categorySet = new Set<string>();
+			response.data.content?.map((item: IGets) => {
+				categorySet.add(item.food_category);
+			});
+			setRightIdx(categorySet.size);
+			setCategory(categorySet);
+			setData(response.data.content);
+			setLoading(false);
+		} catch (e) {
+			setError(e);
+		}
+	};
+
+	mock.onGet("/gets").reply(() => {
+		const gets = {
+			error: null,
+			content: [
+				{
+					food_number: 1,
+					food_category: "버거류",
+					store_id: 1004,
+					food_name: "치즈렐라와퍼",
+					price: 3500,
+					food_pic: "/static/buger1.png",
+					food_explanation: "시원한 아메리카노",
+				},
+				{
+					food_number: 2,
+					food_category: "버거류",
+					store_id: 1004,
+					food_name: "치즈렐라치킨버거",
+					price: 4000,
+					food_pic: "/static/buger2.png",
+					food_explanation: "따뜻한 카페라떼",
+				},
+				{
+					food_number: 3,
+					food_category: "버거류",
+					store_id: 1004,
+					food_name: "몬스터X",
+					price: 3000,
+					food_pic: "/static/buger3.png",
+					food_explanation: "따듯한 녹차",
+				},
+				{
+					food_number: 1,
+					food_category: "버거류",
+					store_id: 1004,
+					food_name: "통새우X",
+					price: 3500,
+					food_pic: "/static/buger4.png",
+					food_explanation: "시원한 아메리카노",
+				},
+				{
+					food_number: 2,
+					food_category: "버거류",
+					store_id: 1004,
+					food_name: "콰트로치즈X",
+					price: 4000,
+					food_pic: "/static/buger5.png",
+					food_explanation: "따뜻한 카페라떼",
+				},
+				{
+					food_number: 3,
+					food_category: "버거류",
+					store_id: 1004,
+					food_name: "기네스콰트로치즈와퍼",
+					price: 3000,
+					food_pic: "/static/buger6.png",
+					food_explanation: "따듯한 녹차",
+				},
+				{
+					food_number: 1,
+					food_category: "버거류",
+					store_id: 1004,
+					food_name: "기네스머쉬룸와퍼",
+					price: 3500,
+					food_pic: "/static/buger7.png",
+					food_explanation: "시원한 아메리카노",
+				},
+				{
+					food_number: 2,
+					food_category: "버거류",
+					store_id: 1004,
+					food_name: "기네스와퍼",
+					price: 4000,
+					food_pic: "/static/buger8.png",
+					food_explanation: "따뜻한 카페라떼",
+				},
+				{
+					food_number: 3,
+					food_category: "버거류",
+					store_id: 1004,
+					food_name: "몬스터와퍼",
+					price: 3000,
+					food_pic: "/static/buger9.png",
+					food_explanation: "따듯한 녹차",
+				},
+				{
+					food_number: 1,
+					food_category: "사이드",
+					store_id: 1004,
+					food_name: "아메리카노",
+					price: 3500,
+					food_pic: "이미지 주소",
+					food_explanation: "시원한 아메리카노",
+				},
+				{
+					food_number: 2,
+					food_category: "음료",
+					store_id: 1004,
+					food_name: "카페라떼",
+					price: 4000,
+					food_pic: "이미지 주소",
+					food_explanation: "따뜻한 카페라떼",
+				},
+				{
+					food_number: 3,
+					food_category: "세트",
+					store_id: 1004,
+					food_name: "녹차",
+					price: 3000,
+					food_pic: "이미지 주소",
+					food_explanation: "따듯한 녹차",
+				},
+			],
+		};
+		return [200, gets];
+	});
+
+	return (
+		<>
+			{loading ? (
+				<Loading />
+			) : (
+				// <Wrapper>
+				// 	<Tab>
+				// 		<button onClick={TabMinus} id="Tab-arrow">
+				// 			<FontAwesomeIcon icon={faAngleLeft} />
+				// 		</button>
+				// 		<ul>{RenderTap()}</ul>
+				// 		<button onClick={TabPlus} id="Tab-arrow">
+				// 			<FontAwesomeIcon icon={faAngleRight} />
+				// 		</button>
+				// 	</Tab>
+				// 	<Menu>{RenderMenu()}</Menu>
+				// 	<Slide>
+				// 		<button onClick={SlideMinus}>
+				// 			<FontAwesomeIcon icon={faCircleArrowLeft} />
+				// 		</button>
+				// 		<ul>{RenderSlide(pagenum)}</ul>
+				// 		<button onClick={SlidePlus}>
+				// 			<FontAwesomeIcon icon={faCircleArrowRight} />
+				// 		</button>
+				// 	</Slide>
+				// 	<Bottom>
+				// 		<div id="total-result">
+				// 			<div>
+				// 				<span>수량</span>
+				// 				<span>{totalnumber}</span>
+				// 			</div>
+				// 			<div>
+				// 				<span>금액</span>
+				// 				<span>{totalprice}</span>
+				// 			</div>
+				// 		</div>
+				// 		<Link href={`/basket`}>
+				// 			<PaymentBascket>
+				// 				<span>장바구니</span>
+				// 			</PaymentBascket>
+				// 		</Link>
+				// 		<Link href={`/payment`}>
+				// 			<PaymentBascket>
+				// 				<span>결제</span>
+				// 			</PaymentBascket>
+				// 		</Link>
+				// 	</Bottom>
+				// </Wrapper>
+				<ConvenientLayout>
+					<ConvenientTitle>
+						<span>메뉴를 선택해주세요</span>
+					</ConvenientTitle>
+					<Main></Main>
+					<Footer>
+						<ConvenientButton color="orange" oper={goPrev}>
+							<span>이전화면으로 돌아가기</span>
+						</ConvenientButton>
+					</Footer>
+				</ConvenientLayout>
+			)}
+		</>
+	);
 }
 
 export default menu;
