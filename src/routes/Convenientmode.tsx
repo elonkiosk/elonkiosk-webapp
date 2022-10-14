f55ae5908096b53044672b003ae5cc2a05849f73
chopinballadeno4 <qo98333@naver.com>
2022-10-15 02:56:23 +0900

    Convenientmode session storage fix

--- src/routes/Convenientmode.tsx
+++ src/routes/Convenientmode.tsx
@@ -1,24 +1,26 @@
 import styled from "@emotion/styled";
 import { useNavigate } from "react-router-dom";
-import React, { useEffect, useState } from "react";
-import Loading from "../components/Loading";
+import React from "react";
 import ConvenientTitle from "../components/ConvenientTitle";
 import ConvenientButton from "../components/ConvenientButton";
 import ConvenientLayout from "../components/ConvenientLayout";
-import { useParams, useLocation } from "react-router";
 
 const Menu = styled.div`
 	background-color: red;
 	display: grid;
 	grid-template-columns: repeat(2, 1fr);
-	//gap: 5px;
 `;
 
 function ConvenientMode() {
 	const navigate = useNavigate();
 
 	const funcIsPickup = (event: React.MouseEvent<HTMLButtonElement>): void => {
-		window.sessionStorage.setItem("ispickup", "yes");
+		if (sessionStorage.getItem("ispickup") === null) {
+			sessionStorage.setItem("ispickup", "yes");
+		} else {
+			sessionStorage.removeItem("ispickup");
+			sessionStorage.setItem("ispickup", "yes");
+		}
 		navigate("/convenientmain");
 		event.preventDefault();
 	};
@@ -26,7 +28,12 @@ function ConvenientMode() {
 	const funcIsNotPickup = (
 		event: React.MouseEvent<HTMLButtonElement>,
 	): void => {
-		window.sessionStorage.setItem("ispickup", "no");
+		if (sessionStorage.getItem("ispickup") === null) {
+			sessionStorage.setItem("ispickup", "no");
+		} else {
+			sessionStorage.removeItem("ispickup");
+			sessionStorage.setItem("ispickup", "no");
+		}
 		navigate("/convenientmain");
 		event.preventDefault();
 	};
@@ -34,9 +41,7 @@ function ConvenientMode() {
 	return (
 		<ConvenientLayout>
 			<ConvenientTitle>
-				<span style={{ fontSize: "30px" }}>
-					{"드시고 가시나요?\n포장해 가시나요?"}
-				</span>
+				<span>{"드시고 가시나요?\n포장해 가시나요?"}</span>
 			</ConvenientTitle>
 			<Menu>
 				<ConvenientButton color="green" oper={funcIsPickup}>
