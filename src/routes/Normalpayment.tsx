eadea921fc9f5277ee9e300641afe828259ded47
chopinballadeno4 <qo98333@naver.com>
2022-10-20 04:45:34 +0900

    normal mode finish .. normal mode code refactoring

--- src/routes/Normalpayment.tsx
+++ src/routes/Normalpayment.tsx
@@ -6,6 +6,7 @@ import {
 	faMobileScreen,
 } from "@fortawesome/free-solid-svg-icons";
 import NormalLayout from "../components/NormalLayout";
+import BasketSynthesis from "../components/BasketSynthesis";
 
 const PaymentWrapper = styled.div`
 	background-color: var(--color-darkwhite);
@@ -25,39 +26,6 @@ const MainItem = styled.main`
 `;
 
 const PaymentMethod = styled.div``;
-
-// const PaymentMethodRadio = styled(Radio)`
-// 	display: flex;
-// 	margin: 25px 0 10px 0;
-// `;
-
-const ElonPay = styled.div`
-	width: 100%;
-	display: grid;
-	place-items: center;
-	margin-top: 20px;
-`;
-
-const ElonPayCard = styled.div`
-	height: 150px;
-	background-color: var(--color-blue);
-	width: 60%;
-	border-radius: 8px;
-	display: flex;
-	align-items: flex-end;
-`;
-
-const ElonPayCardMain = styled.div`
-	background-color: var(--color-darkwhite);
-	width: 100%;
-	height: 95%;
-	border-bottom-right-radius: 8px;
-	border-bottom-left-radius: 8px;
-
-	img {
-		height: 100%;
-	}
-`;
 
 const NormalPay = styled.div`
 	display: grid;
@@ -85,8 +53,6 @@ const NormalPayItem = styled.button<{ isActived?: boolean }>`
 		font-weight: ${props => (props.isActived ? "bold" : "100")};
 	}
 `;
-
-const OrderHistory = styled.div``;
 
 const OrderButton = styled.div`
 	position: fixed;
@@ -118,21 +84,8 @@ const MainHeader = styled.span`
 	font-weight: bold;
 `;
 
-const SubHeader = styled.span`
-	font-size: 16px;
-	letter-spacing: -1;
-`;
-
-const PaymentContent = styled.div`
-	display: flex;
-	justify-content: space-between;
-	margin: 20px 0;
-`;
-
 const Normalpayment = () => {
-	//const radio = useRadioState();
 	const [normalpaymenttype, setNormalPaymentType] = useState("");
-	//console.log(radio);
 
 	return (
 		<NormalLayout>
@@ -141,86 +94,58 @@ const Normalpayment = () => {
 					<PaymentMethod>
 						<MainHeader>결제수단</MainHeader>
 						<div>
-							<input type="radio">일론 페이</input>
-							{radio.state === "ElonPay" ? (
-								<ElonPay>
-									<ElonPayCard>
-										<ElonPayCardMain></ElonPayCardMain>
-									</ElonPayCard>
-								</ElonPay>
-							) : null}
-						</div>
-						<div>
-							<PaymentMethodRadio
-								name="NormalPay"
-								value="NormalPay"
-								bigger
-								{...radio}
-							>
-								일반 결제
-							</PaymentMethodRadio>
-							{radio.state === "NormalPay" ? (
-								<NormalPay>
-									<NormalPayItem
-										onClick={() => setNormalPaymentType("kakaopay")}
-										isActived={normalpaymenttype == "kakaopay" ? true : false}
-									>
-										<span>카카오페이</span>
-									</NormalPayItem>
-									<NormalPayItem
-										onClick={() => setNormalPaymentType("naverpay")}
-										isActived={normalpaymenttype == "naverpay" ? true : false}
-									>
-										<span>네이버페이</span>
-									</NormalPayItem>
-									<NormalPayItem
-										onClick={() => setNormalPaymentType("toss")}
-										isActived={normalpaymenttype == "toss" ? true : false}
-									>
-										<span>토스</span>
-									</NormalPayItem>
-									<NormalPayItem
-										onClick={() => setNormalPaymentType("payco")}
-										isActived={normalpaymenttype == "payco" ? true : false}
-									>
-										<span>페이코</span>
-									</NormalPayItem>
-									<NormalPayItem
-										onClick={() => setNormalPaymentType("card")}
-										isActived={normalpaymenttype == "card" ? true : false}
-									>
-										<FontAwesomeIcon icon={faCreditCard} />
-										<span>신용카드</span>
-									</NormalPayItem>
-									<NormalPayItem
-										onClick={() => setNormalPaymentType("phone")}
-										isActived={normalpaymenttype == "phone" ? true : false}
-									>
-										<FontAwesomeIcon icon={faMobileScreen} />
-										<span>휴대폰결제</span>
-									</NormalPayItem>
-								</NormalPay>
-							) : null}
+							<NormalPay>
+								<NormalPayItem
+									onClick={() => setNormalPaymentType("kakaopay")}
+									isActived={normalpaymenttype == "kakaopay" ? true : false}
+								>
+									<span>카카오페이</span>
+								</NormalPayItem>
+								<NormalPayItem
+									onClick={() => setNormalPaymentType("naverpay")}
+									isActived={normalpaymenttype == "naverpay" ? true : false}
+								>
+									<span>네이버페이</span>
+								</NormalPayItem>
+								<NormalPayItem
+									onClick={() => setNormalPaymentType("toss")}
+									isActived={normalpaymenttype == "toss" ? true : false}
+								>
+									<span>토스</span>
+								</NormalPayItem>
+								<NormalPayItem
+									onClick={() => setNormalPaymentType("payco")}
+									isActived={normalpaymenttype == "payco" ? true : false}
+								>
+									<span>페이코</span>
+								</NormalPayItem>
+								<NormalPayItem
+									onClick={() => setNormalPaymentType("card")}
+									isActived={normalpaymenttype == "card" ? true : false}
+								>
+									<FontAwesomeIcon icon={faCreditCard} />
+									<span>신용카드</span>
+								</NormalPayItem>
+								<NormalPayItem
+									onClick={() => setNormalPaymentType("phone")}
+									isActived={normalpaymenttype == "phone" ? true : false}
+								>
+									<FontAwesomeIcon icon={faMobileScreen} />
+									<span>휴대폰결제</span>
+								</NormalPayItem>
+							</NormalPay>
 						</div>
 					</PaymentMethod>
 				</MainItem>
 				{/* ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━ */}
 				<MainItem style={{ marginTop: "10px" }}>
-					<MainHeader>결제내용</MainHeader>
-					<PaymentContent>
-						<SubHeader>주문수량</SubHeader>
-						<span>3</span>
-					</PaymentContent>
-					<PaymentContent>
-						<SubHeader>결제금액</SubHeader>
-						<span>10,500원</span>
-					</PaymentContent>
+					<BasketSynthesis isConvenient={false} />
 				</MainItem>
 
 				{/* ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━ */}
 				<OrderButton>
 					<button onClick={() => alert("주문이 완료 되었습니다!")}>
-						10,500원 결제하기
+						결제하기
 					</button>
 				</OrderButton>
 			</PaymentWrapper>
